0149ef38fdb2088841be448438850bcece190187
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2017-11-05 15:43:44 -0500

    Add files via upload

---
+++ graph.js
@@ -0,0 +1,294 @@
+var obj = [
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170301",
+			"Total": 4456,
+			"Temp": 43,
+			"Wind Speed": 14,
+			"Weather_Effects": "Fog , Rain , Snow"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170302",
+			"Total": 5258,
+			"Temp": 31,
+			"Wind Speed": 11,
+			"Weather_Effects": "Snow"
+        }, 
+        
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170303",
+			"Total": 5156,
+			"Temp": 27,
+			"Wind Speed": 10
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170304",
+			"Total": 3141,
+			"Temp": 35,
+			"Wind Speed": 15,
+			"Weather_Effects": "Snow"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170305",
+			"Total": 4836,
+			"Temp": 46,
+			"Wind Speed": 15
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170306",
+			"Total": 7121,
+			"Temp": 59,
+			"Wind Speed": 21
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170307",
+			"Total": 7855,
+			"Temp": 55,
+			"Wind Speed": 19,
+			"Weather_Effects": "Rain , Thunderstorm"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170308",
+			"Total": 6622,
+			"Temp": 47,
+			"Wind Speed": 22
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170309",
+			"Total": 6320,
+			"Temp": 36,
+			"Wind Speed": 7
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170310",
+			"Total": 5192,
+			"Temp": 30,
+			"Wind Speed": 15
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170311",
+			"Total": 2352,
+			"Temp": 25,
+			"Wind Speed": 9
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170312",
+			"Total": 2375,
+			"Temp": 27,
+			"Wind Speed": 7,
+			"Weather_Effects": "Snow"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170313",
+			"Total": 2535,
+			"Temp": 28,
+			"Wind Speed": 11,
+			"Weather_Effects": "Fog , Snow"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170314",
+			"Total": 1746,
+			"Temp": 24,
+			"Wind Speed": 14,
+			"Weather_Effects": "Fog , Snow , Thunderstorm"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170315",
+			"Total": 3598,
+			"Temp": 25,
+			"Wind Speed": 12
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170316",
+			"Total": 4989,
+			"Temp": 30,
+			"Wind Speed": 6
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170317",
+			"Total": 4819,
+			"Temp": 43,
+			"Wind Speed": 10,
+			"Weather_Effects": "Rain , Snow"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170318",
+			"Total": 3577,
+			"Temp": 39,
+			"Wind Speed": 13
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170319",
+			"Total": 4483,
+			"Temp": 41,
+			"Wind Speed": 9
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170320",
+			"Total": 6533,
+			"Temp": 46,
+			"Wind Speed": 8,
+			"Weather_Effects": "Rain , Thunderstorm"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170321",
+			"Total": 6920,
+			"Temp": 44,
+			"Wind Speed": 12
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170322",
+			"Total": 5512,
+			"Temp": 33,
+			"Wind Speed": 12
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170323",
+			"Total": 6127,
+			"Temp": 40,
+			"Wind Speed": 13,
+			"Weather_Effects": "Rain , Thunderstorm"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170324",
+			"Total": 10105,
+			"Temp": 62,
+			"Wind Speed": 16,
+			"Weather_Effects": "Rain , Thunderstorm"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170325",
+			"Total": 1675,
+			"Temp": 44,
+			"Wind Speed": 12,
+			"Weather_Effects": "Rain"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170326",
+			"Total": 3463,
+			"Temp": 55,
+			"Wind Speed": 8,
+			"Weather_Effects": "Rain"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170327",
+			"Total": 5846,
+			"Temp": 47,
+			"Wind Speed": 7,
+			"Weather_Effects": "Fog , Rain"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170328",
+			"Total": 6608,
+			"Temp": 43,
+			"Wind Speed": 11
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170329",
+			"Total": 5876,
+			"Temp": 42,
+			"Wind Speed": 12,
+			"Weather_Effects": "Fog , Rain"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170330",
+			"Total": 3195,
+			"Temp": 42,
+			"Wind Speed": 15,
+			"Weather_Effects": "Rain , Thunderstorm"
+		},
+		{
+			"json_featuretype": "Grand_Total_By_Date",
+			"Date": "20170331",
+			"Total": 5115,
+			"Temp": 40,
+			"Wind Speed": 15,
+			"Weather_Effects": "Rain"
+		}
+    ];
+            var start = 20;
+            var yAxislength = 150;
+            var numberSpacing = 10
+            var xAxislength = numberSpacing*32;
+            //var c=document.getElementById("graph");
+            var canvas,ctx;
+
+        function drawGraph () {
+            canvas = document.querySelector("canvas");
+
+            ctx=canvas.getContext("2d");
+            
+            ctx.beginPath();
+            ctx.lineCap = "round";
+            ctx.lineWidth = "2";
+            ctx.moveTo(start,start);
+
+            //y-axis
+            ctx.lineTo(start,yAxislength);
+            
+            //x-axis
+            ctx.lineTo(xAxislength,yAxislength);
+            
+            //lable x-axis
+            ctx.font = "15px arial";
+            ctx.textBaseline="hanging";
+            for(i = 1; i < 32; i++){
+                var date = i.toString();
+                var distance = 5 + (i*numberSpacing);
+                ctx.fillText(date,distance,yAxislength+5)
+            }
+            ctx.fillText("Date",xAxislength/2,yAxislength+20);
+                var xVal = 23;
+                var yVal = (obj[0].Total)/100;
+
+                ctx.moveTo(xVal,yAxislength-yVal);
+
+            for(i = 1; i < 30; i++){
+        
+                yVal = (obj[i].Total)/100;
+                xVal = (30 + ((i)*numberSpacing));
+
+                ctx.lineTo(xVal,yAxislength-yVal);
+            }
+            yVal = (obj[30].Total)/100;
+            xVal = (30 + ((30)*numberSpacing));
+
+            ctx.lineTo(xVal,yAxislength-yVal);
+            
+            
+        
+
+
+
+            //draw
+            ctx.stroke();
+        }

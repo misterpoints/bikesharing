bfcafcdd81bc6ccdf0075fd6fbf6a05e5f9f236e
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2017-11-06 15:07:41 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -71,8 +71,8 @@ function clearMap() {
         onEachFeature: myFunctionHolder.clickMe,
         onEachFeature: myFunctionHolder.addPopups,
         pointToLayer: myFunctionHolder.pointToCircle
-        mapObject.addLayer(stationLayer);
         });
+        mapObject.addLayer(stationLayer);
     }                              
 }
 

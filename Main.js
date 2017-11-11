63605e89553b91246e29042c8961b5d42eedf4ce
Points <eapointer@gmail.com>
2017-11-11 14:29:24 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,11 +1,13 @@
-//"use strict";
-var myFunctionHolder = {};
-var stationName = '';
+   /*-------------------------
+   ------  VARIABLES  ------ 
+   -------------------------*/
+
+// TO DO -- These variables don't seem to be needed. Leaving here just in case. Delete before BETA.   
+/*var stationName = '';
 var mapObject = '';
 var stationLayer = '';
 var pointList = [];
 var clearList = [];
-var thick = 1;
 var outPolyline;
 var inPolyline;
 var stationCheck = true;
@@ -13,20 +15,19 @@ var inFlowCheck = true;
 var outFlowCheck = true;
 var layerGroup = new L.layerGroup();
 var layer
-var lastStation = '';
-var heatmapCheck = true;
-var heatmapLayer;
-var heatmapCfg = ({
-    "radius": .005,
-    "maxOpacity": .5, 
-    "scaleRadius": true, 
-    "useLocalExtrema": true,
-    latField: 'FromLat',
-    lngField: 'FromLng',
-    valueField: 'Total',});
-var heatmapLayer = new HeatmapOverlay(heatmapCfg);
-heatmapLayer.setData(Heatmap);
 
+var heatmapCheck = true;
+var heatmapLayer; */
+
+var lastStation;
+var myFunctionHolder = {};
+
+
+   /*-------------------------
+   ------CREATE POPUPS------ 
+   -------------------------*/
+
+// Function to create the popups
 myFunctionHolder.addPopups = function (feature, layer) {
     if (feature.properties && feature.properties.name) {
         layer.bindPopup("<b>Address:</b> " + feature.properties.name);
@@ -37,6 +38,7 @@ myFunctionHolder.addPopups = function (feature, layer) {
             this.closePopup();
         });
     }
+        // Displayes the station name in stationDiv and calls the function to draw the lines
         layer.on('click', function(e) {
         stationName = feature.properties.name;
         stationID = feature.properties.id;
@@ -45,9 +47,11 @@ myFunctionHolder.addPopups = function (feature, layer) {
     })
 }
 
+   /*-------------------------
+   ------CREATE STATIONS------ 
+   -------------------------*/
 
-
-//declaring function 2
+// Funcion to create the details for the station markers
 myFunctionHolder.pointToCircle = function (feature, latlng) {
     var geojsonMarkerOptions = {
         radius: 5,
@@ -61,6 +65,11 @@ myFunctionHolder.pointToCircle = function (feature, latlng) {
     return circleMarker;
 }
 
+   /*-------------------------
+   ------ MAP OPTIONS  ------- 
+   -------------------------*/
+
+// Check boxES (index) calls this function to remove elements from the map
 function checkMark () {
     switch (document.getElementById("stationCheck").checked){
         case true:
@@ -89,19 +98,50 @@ function checkMark () {
             outFlowCheck = false;
             break;
     }
-    switch (document.getElementById("heatmapCheck").checked){
-        case true:
-            mapObject.addLayer(heatmapLayer);
-            heatmapCheck = true;
-            break;
-        case false:
-            mapObject.removeLayer(heatmapLayer);
-            heatmapCheck = false;
-            break;
-    }
+};
+
+   /*-------------------------
+   ------   HEAT MAP   ------- 
+   -------------------------*/
+  // TO DO ---- add a case so if ONE box is unchecked it won't remove it 
+
+var heatmapCfg = ({
+    "radius": .005,
+    "maxOpacity": .5, 
+    "scaleRadius": true, 
+    "useLocalExtrema": false,
+    latField: 'lat',
+    lngField: 'lng',
+    valueField: 'value'});
+
+var heatmapLayer = new HeatmapOverlay(heatmapCfg);
+
+// Check box (index) calls this function to draw the TO heatmap
+function heatMapTo(stationID) {
+    if (document.getElementById("heatmapToCheck").checked) {
+    heatmapLayer.setData(toTotals);
+    mapObject.addLayer(heatmapLayer); }
+    else {
+        mapObject.removeLayer(heatmapLayer);
+        heatmapCheck = false; }
+}
+
+// Check box (index) calls this function to draw the FROM heatmap
+function heatMapFrom(stationID) {
+    if (document.getElementById("heatmapFromCheck").checked) {
+    heatmapLayer.setData(fromTotals);
+    mapObject.addLayer(heatmapLayer);}
+    else {
+        mapObject.removeLayer(heatmapLayer);
+        heatmapCheck = false;}
 }
 
 
+   /*-------------------------
+   ------   CLEAR MAP  ------- 
+   -------------------------*/
+
+// This function removes all the layers from the map, then adds the stations back on (it removes them too) 
 function clearMap() {
     for(i in mapObject._layers) {
         if(mapObject._layers[i]._path != undefined) {
@@ -123,49 +163,72 @@ function clearMap() {
     }                              
 }
 
+   /*-------------------------
+   ------  DRAW LINES  ------- 
+   -------------------------*/
+// TO DO ---- work on thickness level max is tappers out after 100 (max 150)    
 
-
+// Function which is called when a station is clicked (.addPopups) to draw the lines
 function stationInteraction(ID) {
-clearMap(mapObject);
-if  (lastStation != ID)  {
-    for (i = 0; i < lines.length; i++) {
-        if (lines[i]['From'] == ID && outFlowCheck) {
-            var x3 = lines[i]['json_geometry']['coordinates'][0][1];
-            var y3 = lines[i]['json_geometry']['coordinates'][0][0];
-            var x4 = lines[i]['json_geometry']['coordinates'][1][1];
-            var y4 = lines[i]['json_geometry']['coordinates'][1][0];
-            pointList= [[x3, y3], [x4, y4]];
-            thick = 1;
-            if (lines[i]['Total'] > 20) {
-                var thick = 10; }
-            outPolyline = L.polyline(pointList,  {color: 'red', weight: thick, interactive: false, opacity:.9}).addTo(mapObject);
-        }
-        if (lines[i]['To'] == ID && inFlowCheck) {
-            var x3 = lines[i]['json_geometry']['coordinates'][0][1];
-            var y3 = lines[i]['json_geometry']['coordinates'][0][0];
-            var x4 = lines[i]['json_geometry']['coordinates'][1][1];
-            var y4 = lines[i]['json_geometry']['coordinates'][1][0];
-            pointList= [[x3, y3], [x4, y4]];
-            thick = 1;
-            if (lines[i]['Total'] > 20) {
-                var thick = 10; 
+    
+    clearMap(mapObject); // Clears any lines which are on the map
+    if  (lastStation != ID)  { // Checks to see if the user clicked on the same station or not
+        for (i = 0; i < lines.length; i++) {
+            if (lines[i]['From'] == ID && outFlowCheck) { // Checks to see if the checkbox is checked or not
+                var x3 = lines[i]['json_geometry']['coordinates'][0][1];
+                var y3 = lines[i]['json_geometry']['coordinates'][0][0];
+                var x4 = lines[i]['json_geometry']['coordinates'][1][1];
+                var y4 = lines[i]['json_geometry']['coordinates'][1][0];
+                pointList= [[x3, y3], [x4, y4]];
+                var thick = 1; // Resets thickness to 1
+                if (lines[i]['Total'] > 20) { // Sets the thickness of the line depending on the amount of interaction
+                    thick = 10; 
+                }
+                outPolyline = L.polyline(pointList,  {color: 'red', weight: thick, interactive: false, opacity:.9}).addTo(mapObject);
             }
-           inPolyline = L.polyline(pointList, {color: 'green', weight: thick, interactive: false, opacity:.9}).addTo(mapObject);
+            if (lines[i]['To'] == ID && inFlowCheck) { // Checks to see if the checkbox is checked or not
+                var x3 = lines[i]['json_geometry']['coordinates'][0][1];
+                 var y3 = lines[i]['json_geometry']['coordinates'][0][0];
+                var x4 = lines[i]['json_geometry']['coordinates'][1][1];
+                var y4 = lines[i]['json_geometry']['coordinates'][1][0];
+                pointList= [[x3, y3], [x4, y4]];
+                thick = 1; // Resets thickness to 1
+                if (lines[i]['Total'] > 20) { // Sets the thickness of the line depending on the amount of interaction
+                    var thick = 10; 
+                }
+                inPolyline = L.polyline(pointList, {color: 'green', weight: thick, interactive: false, opacity:.9}).addTo(mapObject);
+            }
+        lastStation = ID // Used for 2nd line of this function
         }
-        lastStation = ID
     }
-}
-else {
-lastStation = '';
-}
-}
+    else {
+        lastStation = ''; // Used for 2nd line of this function
+    }
+};
+
+   /*-------------------------
+   ------ INSTRUCTIONS ------- 
+   -------------------------*/
+
+// Creates a pop up to display the instructions
+function instructions() {
+    alert("This web app shows interaction between DIVVY's bike stations throughout Chicago.\n \n" +
+         "To use this app: \nClick on a station in the map. Two colored lines will appear, green and red. These lines represent the inflow and outflow of bikes from that station during March 2017."
+        + "The thicker the line, the more interaction there was between the two stations. Click on the same station again to remove the lines. \nAfter you click on a station the graph will show the actual numbers of in and out for that station." +
+        " Highlighting a day in the graph will also display weather for that day. \n\nOther options for this map are below the graph, you can turn on or off the stations or in/out flow. " +
+        "Additionally, you can turn on a heat map to show which stations had the highest interaction overall for either bikes coming in or going out. ");            
+    }     
 
 
-       
-  
-//execute
+   /*-------------------------
+   ------   ON LOAD   ------- 
+   -------------------------*/
+
+// Loads the map, chart, and stations when the page is loaded   
 window.onload = function () {
-    renderMyChart();
+
+    renderMyChart() // Calls the function to draw the chart located in barChart.js
+
     mapObject = L.map('mapDiv');
 
     var baseMap = L.tileLayer('https://api.mapbox.com/styles/v1/sinba/ciperkjzk001jb6mdcb41o922/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2luYmEiLCJhIjoiY2loMWF6czQxMHdwcnZvbTNvMjVhaWV0MyJ9.zu-djzdfyr3C_Uj2F7noqg', {
@@ -174,22 +237,22 @@ window.onload = function () {
         attribution: "&copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
     }).addTo(mapObject);
 
+    // Creates the stations later from stations.js    
     stationLayer = L.geoJSON(Stations, {
-        onEachFeature: myFunctionHolder.clickMe,
+        //onEachFeature: myFunctionHolder.clickMe,
         onEachFeature: myFunctionHolder.addPopups,
         pointToLayer: myFunctionHolder.pointToCircle
     });
     
+    // Adds the stations to the map
     if (document.getElementById("stationCheck").checked == true) {
         mapObject.addLayer(stationLayer); 
         stationCheck = true;
        }; 
     
-    if (document.getElementById("heatmapCheck").checked == true) {
-        mapObject.addLayer(heatmapLayer);}
-
+    // Sets the bounds of the map
     mapObject.fitBounds(stationLayer.getBounds());
-   
 
-    
+    // Calls the function to display the instructions   
+    instructions();
 };

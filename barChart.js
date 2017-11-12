f360a31e991a23bae092e87626b1099cbd3d9d03
Points <eapointer@gmail.com>
2017-11-12 12:40:49 -0500

    Update barChart.js

--- barChart.js
+++ barChart.js
@@ -19,7 +19,7 @@ function triggerWeatherData(date) {
         else {
             tempDate = tempDate.slice(6)
         }
-        if (date == tempDate) {
+        if (d.Date == tempDate) {
             document.getElementById("weatherDiv").innerHTML =
             ("The weather for March " + tempDate + " was " + weather[i]['Temp'] + " degrees" +
             ", had " + weather[i]['Wind Speed'] + "mph winds, and had " + weather[i]['Weather_Effects']);
@@ -30,63 +30,78 @@ function triggerWeatherData(date) {
    /*-------------------------
    ------  BAR CHART   ------ 
    -------------------------*/
+// TO DO - Implement the triggerWeatherData function
 
-// Function, called by Main.js, to create the chart. Uses totals.csv
-function renderMyChart() {
-    var svg = d3.select("svg"),
-        margin = { top: 20, right: 20, bottom: 30, left: 70 },
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom;
+   
+// Function, called by Main.js, to create the chart. Uses data.txt
+function DrawChart(ID) {
 
-    var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-        y = d3.scaleLinear().rangeRound([height, 0]);
+    d3.tsv("data.txt", type, function(error, data) {
+        x.domain(data.map(function(d) { return d.Date; }));
+        y.domain([0, d3.max(data, function(d) { return d[ID]; })]);
 
-    var g = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
 
-    // change the dataset
-    d3.csv("totals.csv", function (d) {
-        // change the y value
-        d.count = +d.count;
-        return d;
-    }, function (error, data) {
-        if (error) throw error;
+        chart.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
 
-        x.domain(data.map(function (d) { return d.date; }));
-        y.domain([0, d3.max(data, function (d) { return d.count; })]);
-
-        g.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
-
-        g.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y).ticks(10))
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "0.71em")
-            .attr("text-anchor", "end")
-            .text("Population");
-
-        g.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function (d) { return x(d.date); })
-            .attr("y", function (d) { return y(d.count); })
-            .attr("width", x.bandwidth())
-            .attr("height", function (d) { return height - y(d.count); })
-            .attr("id", function (d) { return d.date; })
-
-        .on("mouseover", function (d) {
-            triggerWeatherData(d.date);
-        })
-        
-        .on("mouseout", function (d) {
-            triggerWeatherReset();
-        })
-
+        chart.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.Date); })
+        .attr("y", function(d) { return y(d[ID]); })
+        .attr("height", function(d) { return height - y(d[ID]); })
+        .attr("width", x.rangeBand());
     });
+
+    function type(d) {
+        d[ID] = +d[ID]; // coerce to number
+        return d;
+    } 
+};
+
+function DrawChart2(ID) {
+
+    d3.tsv("data.txt", type, function(error, data) {
+        y.domain([0, d3.max(data, function(d) { return d[ID]; })]);
+
+    // Remove previous y-axis:
+        chart.select(".y.axis").remove(); // << this line added
+        // Existing code to draw y-axis:
+        chart.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Totals");
+
+        var bar = chart.selectAll(".bar")
+        .data(data, function(d) { return d.Date; });
+
+        bar.enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.Date); })
+        .attr("y", function(d) { return y(d[ID]); })
+        .attr("height", function(d) { return height - y(d[ID]); })
+        .attr("width", x.rangeBand());
+        // removed data:
+        bar.exit().remove();
+
+        bar.transition().duration(750)  // <<< added this
+        .attr("y", function(d) { return y(d[ID]); })
+        .attr("height", function(d) { return height - y(d[ID]); });   
+    });
+
+    function type(d) {
+        d[ID] = +d[ID]; // coerce to number
+     return d;
+    } 
 };
